9b058934d555f26cf7b5764d6fff7a3882befb2c
Astrale <ash4567@outlook.fr>
2023-09-27 23:29:55 +0200

    tweb update

--- misc/tweb/src/lib.rs
+++ misc/tweb/src/lib.rs
@@ -1,18 +1,21 @@
 mod utils;
-use wasm_bindgen::prelude::*;
 use typstfmt_lib::*;
+use wasm_bindgen::prelude::*;
 mod highlight;
 
-
 #[wasm_bindgen]
-pub fn format_default(s : &str) -> String {
+pub fn format_default(s: &str) -> String {
     format(s, Config::default())
 }
 
 #[wasm_bindgen]
-pub fn highlight(s : &str) -> String {
+pub fn highlight(s: &str) -> String {
     let root = typst_syntax::parse(s);
-    
+
     highlight::highlight_html(&root)
 }
 
+#[wasm_bindgen]
+pub fn is_erroneous(s: &str) -> bool {
+    typst_syntax::parse(s).erroneous()
+}
---
+++ static/tweb/tweb.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} s
+* @returns {string}
+*/
+export function format_default(s: string): string;
+/**
+* @param {string} s
+* @returns {string}
+*/
+export function highlight(s: string): string;
+/**
+* @param {string} s
+* @returns {boolean}
+*/
+export function is_erroneous(s: string): boolean;
